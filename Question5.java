64b4e823525e4e37bc4237f97b97992cbb1d6857
ChxCake <64149837+ChxCake@users.noreply.github.com>
2020-04-23 00:57:30 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
@@ -27,6 +28,62 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter number of numbers ;)  :");
+    int numofnum = in.nextInt();
+    ArrayList<Integer> numlist =new ArrayList<Integer>();
     
+    int i =0;
+    while (i < numofnum) 
+    {
+      System.out.println("Enter a number :");
+      int number= in.nextInt();
+      
+      numlist.add(number);
+      i++;
+
+    }
+
+    int maxcount=0; 
+    int mostcount=0;
+
+    for (int x : numlist)
+    {
+      //System.out.println(x);
+      
+      int k = 0;
+      int totalcounter = 0;
+
+      while (k<numofnum)
+      {
+        //System.out.println("no. k loop"+k);
+        
+
+        
+        
+        if(x==numlist.get(k))
+        {
+          totalcounter = totalcounter +1;
+          System.out.println("total counter: "+ totalcounter);
+        }
+        k=k+1;
+        //System.out.println("k end" + k);
+        
+
+      }
+      if (totalcounter > maxcount)
+      {
+        maxcount = totalcounter;
+        mostcount=x;
+      }
+      else if (totalcounter == maxcount)
+      {
+        if(x<mostcount)
+        {
+          mostcount=x;
+        }
+      }
+      
+    }
+    System.out.println("mode is " + mostcount);
   }
 }
