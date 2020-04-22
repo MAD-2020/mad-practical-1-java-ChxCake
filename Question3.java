e6789beabea055364212c655df1118778da84aa0
ChxCake <64149837+ChxCake@users.noreply.github.com>
2020-04-22 23:35:05 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int num = in.nextInt();
+    int answer = num * num;
+    System.out.println("The result is: " + answer);
     
   }
 }
